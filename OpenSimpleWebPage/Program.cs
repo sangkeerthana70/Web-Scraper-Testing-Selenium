20093af15cd689db8a2732ba5731357711a742c3
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-20 11:09:26 -0400

    Testing Selenium

---
+++ OpenSimpleWebPage/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace OpenSimpleWebPage
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.AddArguments("--disable-gpu");
+            var chromeDriver = new ChromeDriver(options);
+            chromeDriver.Navigate().GoToUrl("https://reddit.com");
+                Console.WriteLine("Hello I'm reddit");
+        }
+    }
+}
