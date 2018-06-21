2112d6a68c42082b313052303190f02bcbc57426
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-21 10:41:57 -0400

    Experimenting X-Path

--- OpenSimpleWebPage/Program.cs
+++ OpenSimpleWebPage/Program.cs
@@ -6,15 +6,17 @@ namespace OpenSimpleWebPage
 {
     class Program
     {
-        
-
         static void Main(string[] args)
         {
             var options = new ChromeOptions();
             options.AddArguments("--disable-gpu");
-            var chromeDriver = new ChromeDriver(options);
-            chromeDriver.Navigate().GoToUrl("https://reddit.com");
-                Console.WriteLine("Hello I'm reddit");
+            var chromeDriver = new ChromeDriver(options);//create chrome driver
+            chromeDriver.Navigate().GoToUrl("https://google.com");//navigate to reddit.com
+                Console.WriteLine("Hello I'm google");
+
+             chromeDriver.FindElementByXPath("//*[@id=\"lst-ib\"]").Click();//grab the X-Path to navigate to an element in the reddit page, find it and click on it
+             chromeDriver.Keyboard.SendKeys("cats");
+             chromeDriver.Keyboard.SendKeys(Keys.Enter);//should take you to a new page
         }
     }
 }
