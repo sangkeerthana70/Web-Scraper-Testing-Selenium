45a45e0b1b4916075ffeb8f044f08b856816ae67
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-22 17:11:47 -0400

    Scraped shopping data from google

--- OpenSimpleWebPage/Program.cs
+++ OpenSimpleWebPage/Program.cs
@@ -1,22 +1,37 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 
 namespace OpenSimpleWebPage
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             var options = new ChromeOptions();
             options.AddArguments("--disable-gpu");
             var chromeDriver = new ChromeDriver(options);//create chrome driver
-            chromeDriver.Navigate().GoToUrl("https://google.com");//navigate to reddit.com
-                Console.WriteLine("Hello I'm google");
+            chromeDriver.Navigate().GoToUrl("https://google.com");//navigate to google.com
+                Console.WriteLine("Hello I'm Google");
 
-             chromeDriver.FindElementByXPath("//*[@id=\"lst-ib\"]").Click();//grab the X-Path to navigate to an element in the reddit page, find it and click on it
-             chromeDriver.Keyboard.SendKeys("cats");
-             chromeDriver.Keyboard.SendKeys(Keys.Enter);//should take you to a new page
+
+
+            chromeDriver.FindElementByXPath("//*[@id=\"lst-ib\"]").Click();//grab the X-Path to navigate to an element in the google page, find it and click on it
+            chromeDriver.Keyboard.SendKeys("mountain bike");
+            chromeDriver.Keyboard.SendKeys(Keys.Enter);//should take you to a new page
+
+            chromeDriver.FindElementByXPath("//*[@id=\"hdtb-msb-vis\"]/div[2]/a").Click();
+            var items = chromeDriver.FindElementsByXPath("//*[@id=\"rso\"]/div[1]/div/div[*]/div/div[2]/div/div[1]/a");
+            Console.WriteLine(items.Count);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Text);
+            }
+
         }
     }
 }
