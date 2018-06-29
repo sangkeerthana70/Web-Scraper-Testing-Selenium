2702239c4e6cf1604c6606f7d5b5914b4f3bb81b
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-06-28 20:06:39 -0400

    more progress made

--- YahooFinance/YahooFinance/Program.cs
+++ YahooFinance/YahooFinance/Program.cs
@@ -18,6 +18,7 @@ namespace YahooFinance
         {
             var options = new ChromeOptions();
             options.AddArguments("--disable-gpu");
+            
             var chromeDriver = new ChromeDriver(options);//create chrome driver
             //var wait = new chromeDriverWait(chromeDriver, TimeSpan.FromSeconds(20));
 
@@ -36,6 +37,8 @@ namespace YahooFinance
             //implicitly wait until the next search element is found
             chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             chromeDriver.Url = ("https://finance.yahoo.com/portfolio/p_1/view/v1");
+            options.AddArguments("disable-popup-blocking");//to disable pop-up blocking
+
             chromeDriver.FindElementByXPath("//*[@id=\"uh\"]/header/ul[2]/li[3]/a").Click();
         }
     }
